d3c3b0dc30f25b3f5b5be362d9e189f1e86d44fe
DESKTOP-QTUNK1K\aveti <avetissargsian@gmail.com>
2020-09-30 20:57:00 +0200

    add data transfer for popup manager

--- popup/PopupManager.lua
+++ popup/PopupManager.lua
@@ -7,13 +7,13 @@ function PopupManager.register_popup(id, factory)
 	popups_factorys[id] = factory;
 end
 
-function PopupManager.open_popup(id, callback)
+function PopupManager.open_popup(id, data)
 	if not popups_factorys[id] then return end
 	local collection_popup = collectionfactory.create(popups_factorys[id]);
 	local col_pop_ui = collection_popup[hash("/popup")];
 	local url = msg.url(nil, col_pop_ui, "ui");
 	open_popups[id] = url;
-	msg.post(url, "open_popup")
+	msg.post(url, "open_popup", data);
 end
 
 function PopupManager.close_popup(id)
