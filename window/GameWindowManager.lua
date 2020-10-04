3c244c7046bdaff0cf336f36298e928b3d5ce703
DESKTOP-QTUNK1K\aveti <avetissargsian@gmail.com>
2020-10-04 13:05:49 +0200

    updat window manager

--- window/GameWindowManager.lua
+++ window/GameWindowManager.lua
@@ -6,6 +6,25 @@ GameWindowManager.on_resize = nil;
 GameWindowManager.on_iconfied = nil;
 GameWindowManager.on_deiconified = nil;
 
+local screenW	= tonumber(sys.get_config("display.width"));
+local screenH	= tonumber(sys.get_config("display.height"));
+
+local window_width = nil;
+local window_height = nil;
+local scale = nil;
+local xoffset = nil;
+local yoffset = nil;
+
+local function calculate_params(width, height)
+	window_width = width;
+	window_height = height;
+	scale = math.min( window_width / screenW , window_height / screenH );
+	local projected_width = window_width / (scale or 1)
+	local projected_height = window_height / (scale or 1)
+	xoffset = (projected_width - screenW) / 2
+	yoffset = (projected_height - screenH) / 2
+end
+
 local function window_callback(self, event, data)
 	if event == window.WINDOW_EVENT_FOCUS_LOST then
 		if GameWindowManager.on_focus_lost then
@@ -24,14 +43,32 @@ local function window_callback(self, event, data)
 			GameWindowManager.on_deiconified();
 		end
 	elseif event == window.WINDOW_EVENT_RESIZED then
+		calculate_params(data.width, data.height);
 		if GameWindowManager.on_resize then 
 			GameWindowManager.on_resize(data.width, data.height)
 		end
 	end
 end
 
-function GameWindowManager.init()
-	window.set_listener(window_callback)
+function GameWindowManager.get_offset()
+	return { x = xoffset, y = yoffset };
 end
 
+function GameWindowManager.get_scale()
+	return scale;
+end
+
+function GameWindowManager.get_window_size()
+	return {width = window_width, height = window_height};
+end
+
+function GameWindowManager.validate_input(action)
+	return {x = action.screen_x / scale - xoffset, y = action.screen_y / scale - yoffset}
+end
+
+function GameWindowManager.init_listeners()
+	window.set_listener(window_callback);
+end
+
+calculate_params(window.get_size());
 return GameWindowManager;
