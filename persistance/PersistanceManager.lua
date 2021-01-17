e0a2d93501f00f00d9d479c0d7c46f1e64ee22b9
Avetis Sargsian <avetissargsian@gmail.com>
2021-01-17 19:01:23 +0100

    save data in diferent files

--- persistance/PersistanceManager.lua
+++ persistance/PersistanceManager.lua
@@ -1,26 +1,26 @@
 local PersistanceManager = {}
 
 local GAME_NAME = "NanogramGame";
-local GAME_FILE = "NanogramGameData";
-
-function PersistanceManager.getFileName()
-	return GAME_FILE;
-end
+local GAME_FILE_PREFIX = "NanogramGameData_";
 
 function PersistanceManager.getGameName()
 	return GAME_NAME;
 end
 
-function PersistanceManager.save(leveData)
-	local my_file_path = sys.get_save_file(GAME_NAME, GAME_FILE)
+function PersistanceManager.save(leveData, chapter)
+	chapter = chapter or "";
+	local full_name = GAME_FILE_PREFIX .. chapter;
+	local my_file_path = sys.get_save_file(GAME_NAME, full_name)
 	pprint("SAVE PATH - ", my_file_path)
 	if not sys.save(my_file_path, leveData) then
 		print("Can't save data");
 	end
 end
 
-function PersistanceManager.load()
-	local file_path = sys.get_save_file(GAME_NAME, GAME_FILE)
+function PersistanceManager.load(chapter)
+	chapter = chapter or "";
+	local full_name = GAME_FILE_PREFIX .. chapter;
+	local file_path = sys.get_save_file(GAME_NAME, full_name)
 	local data_table = sys.load(file_path)
 	if not next(data_table) then
 		return nil;
