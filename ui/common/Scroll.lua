ab968f89129996367b7a1a3f55ed3275d169b20a
Игорь Спиридонов <radiot4k@gmail.com>
2021-01-25 01:18:59 +0200

    set scroll callbacks

--- ui/common/Scroll.lua
+++ ui/common/Scroll.lua
@@ -7,6 +7,7 @@ Scroll.strategies = {
 			if value ~= 0 then
 				local current = scroll.get_current_pos();
 				current.y = scroll.calculate_new_position(move);
+				scroll.call_on_pre_update_pos(current);
 				scroll.update_position(current);
 			end
 		end
@@ -26,8 +27,9 @@ Scroll.strategies = {
 			in_progress = true;
 			local current_pos = scroll.get_current_pos();
 			current_pos.y = scroll.calculate_new_position(step * value);
+			scroll.call_on_pre_update_pos(current_pos);
 			scroll.container.animate("position", current_pos, gui.EASING_OUTBACK, 0.4, 0, function()
-				scroll.update_position(current);
+				scroll.update_position(current_pos);
 				in_progress = false;
 			end);
 		end
@@ -49,6 +51,9 @@ Scroll.strategies = {
 function Scroll.new(mask_node_name, container_node_name, height)
 	local this = {};
 
+	local on_pos_update = nil;
+	local on_pre_update_pos = nil;
+
 	local mask = GUI_Box.new(mask_node_name);
 	local scroll_container = GUI_Box.new(container_node_name);
 	this.container = scroll_container;
@@ -59,6 +64,18 @@ function Scroll.new(mask_node_name, container_node_name, height)
 
 	function this.get_current_pos()
 		return container_pos;
+	end
+
+	function this.get_mask_node()
+		return mask;
+	end
+
+	function this.is_on_top()
+		return start_pos_y == container_pos.y;
+	end
+
+	function this.is_on_bottom()
+		return end_pos_y == container_pos.y;
 	end
 
 	function this.calculate_new_position(dy)
@@ -75,10 +92,28 @@ function Scroll.new(mask_node_name, container_node_name, height)
 	function this.update_position(pos)
 		container_pos = pos;
 		scroll_container.set_position(pos);
+		
+		if on_pos_update then
+			on_pos_update(this);
+		end
 	end
 
 	function this.add_content(node)
 		node.set_parent(scroll_container)
+	end
+
+	function this.set_position_update_cb(cb)
+		on_pos_update = cb;
+	end
+
+	function this.call_on_pre_update_pos(next_pos)
+		if on_pre_update_pos then
+			on_pre_update_pos(next_pos);
+		end
+	end
+
+	function this.set_pre_update_position_cb(cb)
+		on_pre_update_pos = cb;
 	end
 
 	function this.on_input(action_id, action)
