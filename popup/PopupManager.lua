4c663024bcad7b78fdd46c9ccf94aab12890a1b7
Игорь Спиридонов <radiot4k@gmail.com>
2021-03-05 00:01:58 +0200

    Popups Queue

--- popup/PopupManager.lua
+++ popup/PopupManager.lua
@@ -2,6 +2,8 @@ local PopupManager = {}
 
 local popups_factorys = {};
 local open_popups = {};
+local queue = {};
+local is_open = false;
 
 function PopupManager.register_popup(id, factory)
 	popups_factorys[id] = factory;
@@ -13,7 +15,28 @@ function PopupManager.open_popup(id, data)
 	local col_pop_ui = collection_popup[hash("/popup")];
 	local url = msg.url(nil, col_pop_ui, "ui");
 	open_popups[id] = {url = url, go = collection_popup};
+	is_open = true;
 	msg.post(url, "open_popup", data);
+end
+
+function PopupManager.add_to_queue(id, data, position)
+	if #queue > 0 then
+		position = 2;
+	end
+	table.insert(queue, position or #queue + 1, {id = id, data = data})
+	PopupManager.check_queue();
+end
+
+function PopupManager.check_queue()
+	if #queue > 0 and not is_open then
+		PopupManager.open_popup(queue[1].id, queue[1].data);
+	end
+end
+
+function PopupManager.delete_queue()
+	table.remove(queue, 1);
+	is_open = false;
+	PopupManager.check_queue();
 end
 
 function PopupManager.close_popup(id)
